3037f1bdd17e47d61805d01ee89c53ad737ce99a
Developer <mugherisbux12@gmail.com>
2023-08-10 07:46:22 +0100

    shahbakhsh

---
+++ app.js
@@ -0,0 +1,23 @@
+let outputScreen = document.getElementById("out-screen");
+
+function getNumber(num) {
+  outputScreen.value += num;
+}
+
+function calculate() {
+  var result = document.getElementById();
+  result.value = eval;
+}
+function calculate() {
+  try {
+    outputScreen.value = eval(outputScreen.value);
+  } catch {}
+}
+
+function Clear() {
+  outputScreen.value = "";
+}
+
+function del() {
+  outputScreen.value = outputScreen.value.slice(0, 1);
+}
